6774048d3be068d2d91045197e52c8b6e536f001
Ilya Hoilik <ilya@hoilik.com>
2022-04-14 13:06:25 +0300

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lang: 'en',
+    title: 'Russian Kit',
+    description: 'Russian Kit for Invision Community 4',
+
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        repo: 'ipshelp/russian-bundle'
+    }
+}
---
+++ docs/.vuepress/dist/assets/index.html.8eae854f.js
@@ -0,0 +1,1 @@
+const t={key:"v-8daa1a0e",path:"/",title:"Russian Kit",lang:"en",frontmatter:{},excerpt:"",headers:[],git:{},filePathRelative:"README.md"};export{t as data};
